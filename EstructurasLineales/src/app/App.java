52dafb0e020abbf3f56b819b32626007ef02d432
Your Name <you@example.com>
2026-06-10 16:10:01 -0500

    Pendiente

--- EstructurasLineales/src/app/App.java
+++ EstructurasLineales/src/app/App.java
@@ -5,8 +5,8 @@ import utils.SignValidator;
 public class App {
     public static void main(String[] args) throws Exception {
         SignValidator inicio = new SignValidator();
-        String cadena1 = "([() clouds ]){}";
-        String cadena2 = "{[(])}";
+        String cadena1 = "([]){}";
+        String cadena2 = "({)}";
         System.out.println("\n--- EJERCICIO 1 VALIDACIÓN DE SIGNOS ---\n");
         System.out.println("Input: " + cadena1);
         System.out.println("Output: " + inicio.isValid(cadena1));
--- EstructurasLineales/src/utils/StackSorter.java
+++ EstructurasLineales/src/utils/StackSorter.java
@@ -1,4 +1,9 @@
 package utils;
+
+import java.util.Stack;
+
 public class StackSorter {
-    
+    public void sortStack(Stack<Integer> stack) {
+        
+    }
 }
